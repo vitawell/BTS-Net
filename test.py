510c2535ddb32edcd9f8f4e07262137c098d36c3
vitawell <41141374+vitawell@users.noreply.github.com>
2022-06-10 20:18:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -32,20 +32,27 @@ model.cuda()
 model.eval()
 
 #test
-test_datasets = ['NJU2K','NLPR','STERE', 'RGBD135', 'LFSD','SIP']
+test_datasets = ['818']
 for dataset in test_datasets:
     save_path = './results/' + dataset
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-    image_root = dataset_path + dataset + '/RGB/'
-    gt_root = dataset_path + dataset + '/GT/'
-    depth_root=dataset_path +dataset +'/depth/'
-    test_loader = test_dataset(image_root, gt_root,depth_root, opt.testsize)
+    image_root = dataset_path + dataset + '/818rgb/'
+    #gt_root = dataset_path + dataset + '/GT/'
+    depth_root=dataset_path +dataset +'/818depth/'
+    
+    #test_loader = test_dataset(image_root, gt_root,depth_root, opt.testsize)
+    test_loader = test_dataset(image_root, depth_root, opt.testsize)
 
+    print(range(test_loader.size))
     for i in range(test_loader.size):
-        image, gt,depth, name, image_for_post = test_loader.load_data()
-        gt = np.asarray(gt, np.float32)
-        gt /= (gt.max() + 1e-8)
+        #image, gt,depth, name, image_for_post = test_loader.load_data()
+        image, depth, name, image_for_post = test_loader.load_data()
+        WW = 1080 #输出时的图片宽度，根据输入图片的宽度修改
+        HH = 1920 #输出时的图片高度
+        
+        #gt = np.asarray(gt, np.float32)
+        #gt /= (gt.max() + 1e-8)
         image = image.cuda()
         depth = depth.cuda()
         torch.cuda.synchronize()
@@ -54,7 +61,8 @@ for dataset in test_datasets:
         torch.cuda.synchronize()
         time_e = time.time()
         print('Speed: %f FPS' % (1 / (time_e - time_s)))
-        res = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
+        #res = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
+        res = F.upsample(res, size=[WW,HH], mode='bilinear', align_corners=False)
         res = res.sigmoid().data.cpu().numpy().squeeze()
         res = (res - res.min()) / (res.max() - res.min() + 1e-8)
         print('save img to: ',os.path.join(save_path, name))
